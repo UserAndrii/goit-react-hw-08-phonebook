e231a9e726c3abd4771af7d5f86019d0bc3325c0
Andrii Gadar <gadar.andre@gmail.com>
2023-06-12 12:08:45 +0300

    add link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,16 +12,19 @@ const Footer = () => {
         </Text>
       </TextWrapper>
       <div>
-        <IconLink href="https://www.linkedin.com/" target="_blank">
+        <IconLink
+          href="https://www.linkedin.com/in/andrii-hadar/"
+          target="_blank"
+        >
           <FaLinkedin size={30} />
         </IconLink>
-        <IconLink href="https://github.com/" target="_blank">
+        <IconLink href="https://github.com/UserAndrii" target="_blank">
           <FaGithub size={30} />
         </IconLink>
-        <IconLink href="https://telegram.org/" target="_blank">
+        <IconLink href="https://t.me/andrii_hadar" target="_blank">
           <FaTelegram size={30} />
         </IconLink>
-        <IconLink href="https://www.instagram.com/" target="_blank">
+        <IconLink href="https://www.instagram.com/gadarandre/" target="_blank">
           <FaInstagram size={30} />
         </IconLink>
       </div>
